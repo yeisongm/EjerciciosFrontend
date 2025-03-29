d5271f425d3186270509ba4fdd8d29ae58dd5c50
Json G Moreno <yeisongm@hotmail.es>
2025-03-29 09:49:37 -0500

    Primera subida del ejercicio frontend de un carnet

---
+++ Scripts/scripts.js
@@ -0,0 +1,8 @@
+JsBarcode("#codigoBarras", "123456789", {
+    format: "CODE128",  // Tipo de código de barras
+    lineColor: "#000",  // Color del código
+    width: 1,           // Ancho de las líneas
+    height: 20,         // Alto del código de barras
+    displayValue: false,  // Mostrar el texto del código
+    background: "transparent" //Sin fondo
+});
